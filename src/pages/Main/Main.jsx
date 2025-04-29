327761c969a57b2fe2b16dcc264b910a2b1e3504
parkjihun22 <eyeful31@naver.com>
2025-04-29 15:40:35 +0900

    feat : 폼수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -499,17 +499,6 @@ const handleInputChange = (e) => {
       onChange={handleInputChange}
       required
     />
-    <label htmlFor="phone">
-      생년월일 <span className={styles.redStar}>*</span>
-    </label>
-    <input
-      type="number"
-      name="birthDate"
-      placeholder="0000-00-00"
-      value={registration.phone}
-      onChange={handleInputChange}
-      required
-    />
 
     <label htmlFor="message">
       문의 내용
@@ -765,17 +754,7 @@ const handleInputChange = (e) => {
       onChange={handleInputChange}
       required
     />
-    <label htmlFor="phone">
-      생년월일 <span className={styles.redStar}>*</span>
-    </label>
-    <input
-      type="number"
-      name="birthDate"
-      placeholder="0000-00-00"
-      value={registration.phone}
-      onChange={handleInputChange}
-      required
-    />
+
 
     <label htmlFor="message">문의 내용</label>
     <textarea
